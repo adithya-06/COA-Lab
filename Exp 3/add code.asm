b9d020729f9d7fef799c4774496e770397e5db46
adithya-06 <adithya.dutt6@gmail.com>
2022-12-20 17:25:43 +0530

    first commit

---
+++ Exp 3/add code.asm
@@ -0,0 +1,17 @@
+data segment
+    a db 15h
+    b db 12h
+    c dw ?
+    data ends
+code segment
+    assume cs: code, ds: data
+    start:
+    mov ax, data
+    mov ds, ax
+    mov al, a
+    mov bl, b
+    add al, bl
+    mov c, ax
+    int 3
+    code ends
+end start
